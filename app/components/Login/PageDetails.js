185afdd0aa31dcff0e67b8dc740ac274a33ed37f
unknown <pebuka351@gmail.com>
2024-01-17 17:05:54 +0100

    Removed Facebook Sign in method

--- app/components/Login/PageDetails.js
+++ app/components/Login/PageDetails.js
@@ -25,21 +25,7 @@ export default function LoginPageDetails() {
         <p className="text-gray-500/90 text-base text-center pb-5">
           We're happy to see you back!
         </p>
-        <div className="w-full flex justify-between flex-wrap gap-y-3">
-          <button className="w-[48%] max-[500px]:w-full h-9 border border-[#2553b4] rounded overflow-hidden flex">
-            <div className="w-1/5 max-[500px]:w-[40px] h-full flex justify-center items-center bg-transparent">
-              <Image
-                src={FacebookImage}
-                alt="FaceBook Imaage"
-                className="w-6/12 h-6/12"
-              />
-            </div>
-            <div className="w-4/5 max-[500px]:w-[calc(100%-40px)] h-full bg-[#2553b4] flex justify-center items-center">
-              <p className="text-white text-xs font-medium">
-                Sign in with Facebook
-              </p>
-            </div>
-          </button>
+        <div className="w-full flex justify-center gap-y-3">
           <button className="w-[48%] max-[500px]:w-full h-9 border border-[#4285f4] rounded overflow-hidden flex">
             <div className="w-1/5 max-[500px]:w-[40px] h-full flex justify-center items-center bg-transparent">
               <Image
