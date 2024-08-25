80a9837023305967263df1bc4a7f6bc3c56ae2bf
Teslim Abdulwahab <tesimune@gmail.com>
2024-08-25 07:22:58 +0100

    Delete .next directory

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/mnt/c/Users/OPE/Desktop/analogue_shifts/resume.analogueshifts.com/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"WsufVCo1n8x4V0LkNYuAIOSGC7f8oJ8fXZI2mpHNghU=\"\n}"
