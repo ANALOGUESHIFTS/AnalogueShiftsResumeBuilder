fcca9c56341e8099c6ac29e8f295181c2b370014
Leanstix <leanstixx@gmail.com>
2024-09-10 11:33:22 +0100

    testing authentication

--- app/login/components/index.js
+++ app/login/components/index.js
@@ -1,16 +1,26 @@
 "use client"; // Mark the component as client-side
 
 import { useEffect, useState } from "react";
-import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 
 export default function ValidateToken() {
   const [loading, setLoading] = useState(true); // To display "Verifying..." text while token is being checked
   const router = useRouter();
 
+  const getCookie = (name) => {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === " ") c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  };
+
   useEffect(() => {
     // Check if the token exists in the cookies
-    const token = Cookies.get("analogueshifts");
+    const token = getCookie("analogueshifts");
 
     if (!token) {
       // If no token, redirect the user to the external auth page
