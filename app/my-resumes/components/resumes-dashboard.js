68188c1df5d2862c723b788ca0eb3e4ff08ea3a8
Leanstix <leanstixx@gmail.com>
2024-09-13 13:57:42 +0100

    testing

--- app/my-resumes/components/resumes-dashboard.js
+++ app/my-resumes/components/resumes-dashboard.js
@@ -23,6 +23,7 @@ export default function Resumes() {
       const res = await fetch("https://api.analogueshifts.app/api/user", {
         method: "GET",
         headers: {
+          "Content-Type": "application/json",
           Accept: "application/json",
           Authorization: `Bearer ${token}`, // Pass the token in the Authorization header
         },
@@ -31,9 +32,6 @@ export default function Resumes() {
       if (!res.ok) throw new Error("Failed to fetch user information");
 
       const data = await res.json();
-
-      console.log(data)
-
       setUser(data); // Store the user details in state
     } catch (error) {
       console.error("Error fetching user data:", error.message);
@@ -42,7 +40,7 @@ export default function Resumes() {
     }
   };
 
-  
+  console.log(user)
 
   if (loading) {
     return <p>Loading...</p>; // Display loading state
@@ -52,7 +50,7 @@ export default function Resumes() {
     <main className="w-full h-auto pt-[80px]">
       <main className="py-10 bg-[rgb(43,58,69)] px-10 max-[800px]:px-5">
         <p className="pb-5 text-base font-semibold text-white">
-          Welcome Back, {data.first_name || "User"}!
+          Welcome Back, {user?.first_name || "User"}!
         </p>
         <p className="pb-10 font-bold text-white text-xl">
           Edit and Download Professional resumes to get your dream Job!
